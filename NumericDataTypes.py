64d7dc815808ca92a39af9bb66f2658c23bfe36b
Andrei <andreitogni@gmail.com>
2022-11-15 20:03:13 +0800

    lab 2-3

---
+++ NumericDataTypes.py
@@ -0,0 +1,9 @@
+x = 1
+y = 2.01
+z = 3j
+a = True
+
+print(str(x) + " is of the data type " + str(type(x)))
+print(str(y) + " is of the data type " + str(type(y)))
+print(str(z) + " is of the data type " + str(type(z)))
+print(str(a) + " is of the data type " + str(type(a)))
---
+++ StringDataTypes.py
@@ -0,0 +1,15 @@
+x = "This is a string."                                    
+print(str(type(x)))         
+print(str(x) + " is of the data type " + str(type(x)))           
+a = "water"
+b = "fall"
+c = a + b
+print(c)   
+
+name = input("What is your name? ")          
+print(name)
+color = input("What is your favorite color? ")                           
+print(color)
+animal = input("What is your favorite animal? ")                                           
+print(animal)  
+print(name + ", you like a " + color+ " "+ animal+".")                
